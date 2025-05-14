8c2ae5a4c352ff3780afeab95c780f2e22047ceb
Alexandra Sia <alexandra.sia@mediavalet.com>
2025-05-14 11:12:33 -0700

    Import antd

--- components/ScheduleContainer.tsx
+++ components/ScheduleContainer.tsx
@@ -1,17 +1,19 @@
-'use client'
+"use client";
 
 import { GameScheduleProvider } from "../contexts/GameScheduleContext";
-import {IGameDate} from "../types/LeagueSchedule";
-
+import { IGameDate } from "../types/LeagueSchedule";
+import { Button } from "antd";
 
 export type ScheduleContainerProps = {
   gameDates: IGameDate[];
-}
+};
 
 const ScheduleContainer = ({ gameDates }: ScheduleContainerProps) => {
-
   return (
     <GameScheduleProvider initialGameItems={gameDates}>
+      <Button type="primary" onClick={() => console.log("Button clicked!")}>
+        Button
+      </Button>
       <div>
         {gameDates.map((game, index) => (
           <div key={index}>{game.gameDate}</div>
